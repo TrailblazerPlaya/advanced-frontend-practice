b87999921113a632025ff643056dd9eb9d048e27
fornara <fornarapro@gmail.com>
2023-09-29 10:40:50 +0300

    add css modules

--- config/build/buildLoader.ts
+++ config/build/buildLoader.ts
@@ -1,8 +1,24 @@
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import webpack from "webpack";
-export function buildLoader(): webpack.RuleSetRule[] {
+import { BuildOptions } from "./types/config";
+export function buildLoader({ isDev }: BuildOptions): webpack.RuleSetRule[] {
   const cssLoader = {
     test: /\.s[ac]ss$/i,
-    use: ["style-loader", "css-loader", "sass-loader"],
+    use: [
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: (resPath: string) => Boolean(resPath.includes(".module.")),
+            localIdentName: isDev
+              ? "[path][name]__[local]--[hash:base64:5]"
+              : "[hash:base64:8]",
+          },
+        },
+      },
+      "sass-loader",
+    ],
   };
   const typescriptLoader = {
     test: /\.tsx?$/,
