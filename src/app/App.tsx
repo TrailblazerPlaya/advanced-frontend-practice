749b08324ad470ba5b5ce9b8756d78cbad41c86a
fornara <fornarapro@gmail.com>
2023-10-02 18:02:53 +0300

    Navbar
    Template for dev.
    First Ui Kit component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "./providers/ThemeProvider";
 import AppRouter from "./providers/router";
+import { Navbar } from "widgets/Navbar";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
   return (
     <div className={classNames("app", {}, [theme])}>
       <button onClick={toggleTheme}>Toggle Theme</button>
-      <Link to={"/"}>Home</Link>
-      <Link to={"/about"}>About</Link>
+      <Navbar />
       <AppRouter />
     </div>
   );
