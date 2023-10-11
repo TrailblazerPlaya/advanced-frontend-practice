93c3832026f68621048a117765faa1f4fb6bf6e0
fornara <fornarapro@gmail.com>
2023-10-11 16:36:12 +0300

    add NotFound page | add Loader

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,21 +1,24 @@
 import { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import { routeConfig } from "shared/config/routeConfig/routeConfig";
+import { PageLoader } from "widgets/PageLoader";
 
 const AppRouter = () => {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Routes>
-        {Object.entries(routeConfig).map(([key, value]) => (
-          <Route
-            key={key}
-            element={<div className="page-wrapper">{value.element}</div>}
-            path={value.path}
-          />
-        ))}
-      </Routes>
-    </Suspense>
-  );
+    return (
+        <Suspense fallback={<PageLoader />}>
+            <Routes>
+                {Object.entries(routeConfig).map(([key, value]) => (
+                    <Route
+                        key={key}
+                        element={
+                            <div className="page-wrapper">{value.element}</div>
+                        }
+                        path={value.path}
+                    />
+                ))}
+            </Routes>
+        </Suspense>
+    );
 };
 
 export default AppRouter;
--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,23 +1,30 @@
 import { AboutPage } from "pages/AboutPage";
 import { MainPage } from "pages/MainPage";
+import { NotFound } from "pages/NotFound";
 import { type RouteProps } from "react-router-dom";
 export enum AppRouter {
-  MAIN = "main",
-  ABOUT = "about",
+    MAIN = "main",
+    ABOUT = "about",
+    NOT_FOUND = "not_found",
 }
 
 export const RoutePath: Record<AppRouter, string> = {
-  [AppRouter.MAIN]: "/",
-  [AppRouter.ABOUT]: "/about",
+    [AppRouter.MAIN]: "/",
+    [AppRouter.ABOUT]: "/about",
+    [AppRouter.NOT_FOUND]: "*",
 };
 
 export const routeConfig: Record<AppRouter, RouteProps> = {
-  [AppRouter.MAIN]: {
-    path: RoutePath[AppRouter.MAIN],
-    element: <MainPage />,
-  },
-  [AppRouter.ABOUT]: {
-    path: RoutePath[AppRouter.ABOUT],
-    element: <AboutPage />,
-  },
+    [AppRouter.MAIN]: {
+        path: RoutePath[AppRouter.MAIN],
+        element: <MainPage />,
+    },
+    [AppRouter.ABOUT]: {
+        path: RoutePath[AppRouter.ABOUT],
+        element: <AboutPage />,
+    },
+    [AppRouter.NOT_FOUND]: {
+        path: RoutePath[AppRouter.NOT_FOUND],
+        element: <NotFound />,
+    },
 };
